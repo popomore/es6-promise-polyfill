f6fb60f072f080066f55bb134e8ccc24fe237983
余化 <shuai.shao@alipay.com>
2015-01-27 15:33:23 +0800

    init 1.0.0

---
+++ examples/index.md
@@ -0,0 +1,9 @@
+# Demo
+
+---
+
+## Normal usage
+
+````javascript
+require('index');
+````
---
+++ tests/index-spec.js
@@ -0,0 +1,10 @@
+var expect = require('expect.js');
+var es6PromisePolyfill = require('../index');
+
+describe('es6-promise-polyfill', function() {
+
+  it('normal usage', function() {
+
+  });
+
+});
